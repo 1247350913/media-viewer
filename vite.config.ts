e2b95612df35871a964a6b5137ee007ff8aef873
Cameron Hirsh <camjhirsh@gmail.com>
2025-10-10 20:08:35 -0400

    almost there

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import path from "node:path";
+import react from '@vitejs/plugin-react'
+import path from 'node:path'
+import { defineConfig } from 'vite'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist/src',     
+    emptyOutDir: true,      
+  },
   resolve: {
     alias: {
-      "@shared": path.resolve(__dirname, "shared")
-    }
-  }
-});
+      '@shared': path.resolve(__dirname, 'shared'),
+    },
+  },
+})
